435887eaa2c0c3895fba16b854a189201503b4bd
Salomao Souza <souza95salomao@gmail.com>
2021-02-23 11:03:26 -0400

    [REFACTOR] Moving from Pure ReactJS to NextJS.

---
+++ src/components/Experiencebar.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+export function Experiencebar() {
+  return (
+    <header className="experiencebar">
+      <span>0 xp</span>
+      <div >
+        <div style={{ width: '50%' }}/>
+
+        <span className="current-experience" style={{ left: '50%' }}>
+          380 xp
+        </span>
+      </div>
+      <span>680 xp</span>
+    </header>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import {Experiencebar } from '../components/Experiencebar';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Experiencebar />
+    </div>
+  )
+}
