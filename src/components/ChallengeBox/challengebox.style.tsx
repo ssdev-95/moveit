3bacec0dfb5acfd39ced3b3de5ad78a92fa6e0fd
xSallus <souza95salomao@gmail.com>
2021-05-18 14:29:08 -0400

    [UPDATE] ChallengeBox component

---
+++ src/components/ChallengeBox/challengebox.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { white } from 'root/colors.json'
+
+export const ChallengeContainer = styled.div`
+    width: 468px;
+    height: 500px;
+    border-radius: 6px;
+    background: ${white};
+`
