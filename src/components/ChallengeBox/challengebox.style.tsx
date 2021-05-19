fd13516a13e700fd5af893d33382cfb3a3568044
xSallus <souza95salomao@gmail.com>
2021-05-19 10:28:45 -0400

    [UPDATE] ChallengesBox

--- src/components/ChallengeBox/challengebox.style.tsx
+++ src/components/ChallengeBox/challengebox.style.tsx
@@ -6,4 +6,28 @@ export const ChallengeContainer = styled.div`
     height: 500px;
     border-radius: 6px;
     background: ${white};
+    padding: 3rem 2rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+
+    img {
+        height: 6rem;
+    }
+
+    span:first-child {
+        font-size: 1.5rem;
+        font-weight: 500;
+        width: 226px;
+        text-align: center;
+    }
+
+    span:last-child {
+        font-size: 1rem;
+        font-weight: 400;
+        width: 226px;
+        text-align: center;
+    }
 `
--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -1,6 +1,16 @@
+import React from 'react'
+import { ChallengeContainer } from '@/components/ChallengeBox/challengebox.style'
+
 export default function ChallengeBox() {
+    const emptyChallenges = (
+        <>
+            <span>Start a cycle to receive challenges</span>
+            <img src="icons/level-up.svg" alt="Empty challenge" />
+            <span>Go up a level by completing challenges</span>
+        </>
+    )
 
     return (
-        <div></div>
+        <ChallengeContainer>{emptyChallenges}</ChallengeContainer>
     );
 }
