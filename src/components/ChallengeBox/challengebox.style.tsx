9c17256426c0998a216efdfb70a6ec1b404e0c9a
xSallus <souza95salomao@gmail.com>
2021-05-20 13:04:55 -0400

    [UPDATE] Challenge box styles

--- src/components/ChallengeBox/challengebox.style.tsx
+++ src/components/ChallengeBox/challengebox.style.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { white } from 'root/colors.json'
+import { white, blue, red, green, gray_line } from 'root/colors.json'
 
 export const ChallengeContainer = styled.div`
     width: 468px;
@@ -12,6 +12,7 @@ export const ChallengeContainer = styled.div`
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
+    gap: 1rem;
 
     img {
         height: 6rem;
@@ -29,5 +30,57 @@ export const ChallengeContainer = styled.div`
         font-weight: 400;
         width: 226px;
         text-align: center;
+    }
+`
+
+export const Header = styled.header`
+    width: 100%;
+    margin-top: -2rem;
+    padding-bottom: 1.75rem;
+    display: flex;
+
+    align-items: center;
+    justify-content: center;
+
+    border-bottom: 1px solid ${gray_line};
+
+    span {
+        color: ${blue};
+        font-size: 2rem;
+    }
+`
+
+export const ChallengeDetail = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const Actions = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+
+    button {
+        width: 110px;
+        height: 40px;
+        color: white;
+        border-width: 0;
+        border-radius: 4px;
+
+        &:first-child {
+            background: ${red}
+        }
+
+        &:last-child {
+            background: ${green};
+        }
+
+        &:hover {
+            filter: brightness(.89);
+        }
     }
 `
