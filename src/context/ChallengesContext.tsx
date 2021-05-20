c5eb9efc96c69d94fa7fd7e08fd87299548e696b
xSallus <souza95salomao@gmail.com>
2021-05-20 13:05:49 -0400

    [UPDATE] Challenges context

--- src/context/ChallengesContext.tsx
+++ src/context/ChallengesContext.tsx
@@ -1,14 +1,58 @@
-import { createContext, useState, useEffect, useContext } from 'react'
-import { ChallengesContextData, ChallengesProviderProps } from '@/types'
+import { createContext, useState, useContext, useEffect } from 'react'
+import axios from 'axios'
 
-export const ChallengesContext = createContext({} as ChallengesContextData);
+import { ChallengesContextData, ChallengesProviderProps, Challenge } from '@/types'
 
-export function ChallengesProvider({ 
-    children
-}: ChallengesProviderProps) {
+const ChallengesContext = createContext({} as ChallengesContextData);
+
+export const  ChallengesProvider = ({ children}: ChallengesProviderProps) => {
+    const [challenge, setChallenge] = useState<Challenge>({})
+    const [hasChallenge, setHasChallenge] = useState(false)
+    // const types = [
+    //     "busywork",
+    //     "charity",
+    //     "cooking",
+    //     "diy",
+    //     "education",
+    //     "music",
+    //     "recreational",
+    //     "relaxation",
+    //     "social"
+    // ]
+
+    const parseChallenge = async () => {
+        const uri = process.env.API_URI
+
+        const { data } = await axios.get(uri)
+
+        const challenge:Challenge = {
+            amount: (Number(data.accessibility)+Number(data.price))*100,
+            description: data.activity,
+            uuid: data.key,
+            type: data.type,
+            pic: `icons/${data.type}.svg`
+        }
+        
+        setChallenge(challenge)
+    }
+
+    useEffect(()=>{
+        parseChallenge()
+        setHasChallenge(!hasChallenge)
+    }, [])
+
     return (
-        <ChallengesContext.Provider value={{}}>
+        <ChallengesContext.Provider
+          value={{
+              hasChallenge,
+              challenge,
+              parseChallenge
+          }}>
             { children }
         </ChallengesContext.Provider>
     );
+}
+
+export default function useChallenge() {
+    return useContext(ChallengesContext)
 }
