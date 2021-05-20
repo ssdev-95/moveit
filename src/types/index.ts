d215ddd4b6d1f59d0a1086395a444ca3ab6ad5b2
xSallus <souza95salomao@gmail.com>
2021-05-20 15:31:35 -0400

    [UPDATE] Types

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,10 +8,20 @@ export interface Challenge {
   pic?: string;
 }
 
+export interface User {
+  name: string;
+  currentLevel: number;
+  currentXP: number;
+  profilePic: string;
+  challengesCompleted: number;
+  nextLevelXP: number;
+}
+
 export interface ChallengesContextData {
   hasChallenge: boolean;
   challenge: Challenge;
-  parseChallenge: ()=>Promise<void>;
+  initChallenge: ()=>void;
+  handleChallengeEnd: (XPEarned:number, isChallengeCompleted:boolean)=>void;
 }
 
 export interface ChallengesProviderProps {
@@ -27,13 +37,38 @@ export interface CountdownProviderProps {
   children: ReactNode;
 }
 
-export interface HomeProps {}
+export interface HomeProps {
+  user: User;
+}
 
 export interface LevelUpModalContextData {
+  currentLevel: number;
   isLevelUp: boolean;
+  upALevel: ()=>void;
   toggleLevelUpModal: ()=>void;
 }
 
 export interface LevelUpModalProviderProps {
   children: ReactNode;
+}
+
+export interface ProfileData {
+  updateProfile: (level:number, xp:number, numberOfChallenges:number)=>void;
+}
+
+export interface ProfileProviderProps {
+  children: ReactNode;
+}
+
+export interface ProfileProps {
+  user: User;
+}
+
+export interface DashboardProps {
+  user: User;
+}
+
+export interface ExperiencBarProps {
+  currentXP: number;
+  nextLevelXP: number;
 }
