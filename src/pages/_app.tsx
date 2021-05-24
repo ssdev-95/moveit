6dab180c34e8acea37789c9995de3798054fd237
xSallus <souza95salomao@gmail.com>
2021-05-24 09:48:48 -0400

    [UPDATE] Moved GlobalStyle to its own file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { createGlobalStyle } from 'styled-components'
 import React from 'react'
 
 import { LevelUpModalContextProvider } from '@/context/LevelUpModalContext'
@@ -6,52 +5,8 @@ import { ChallengesProvider } from '@/context/ChallengesContext'
 import { CountdownProvider } from '@/context/CountdownContext'
 import { ProfileProvider } from '@/context/ProfileContext'
 
-import { background, text } from 'root/colors.json'
-
 import { CookiesProvider } from 'react-cookie'
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
-  }
-
-  body {
-    width: 100vw;
-    height: 100vh;
-  }
-
-  @media (max-width: 1080px) {
-    html {
-      font-size: 93.75%;
-    }
-  }
-
-  @media (max-width: 720px) {
-    html {
-      font-size: 87.5%;
-    }
-  }
-
-  body {
-    background: ${background};
-    color: ${text};
-  }
-
-  body, input, textarea, button {
-    font: 400 1rem "Inter", sans-serif;
-  }
-
-  button {
-    cursor: pointer;
-  }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-`
+import { GlobalStyle } from '@/styles/globals.style'
 
 function MyApp({ Component, pageProps }) {
   return (
